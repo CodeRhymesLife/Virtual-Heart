daa8fb3ca0b15890affa4bd854094faa9479cd88
rcjames1004 <rcjames1004@gmail.com>
2016-01-02 00:49:45 -0800

    Update models and create heart prefab

---
+++ Virtual Heart (Unity)/Assets/blueprints/organ.cs
@@ -0,0 +1,21 @@
+﻿using SimpleJSON;
+using System;
+using System.Collections;
+using System.IO;
+using UnityEngine;
+
+
+public class organ : MonoBehaviour {
+
+    public int FMAId = -1;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
