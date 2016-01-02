670bacfbc848d04cccaf6c0c296f725b1579da2b
rcjames1004 <rcjames1004@gmail.com>
2016-01-02 01:25:27 -0800

    Read in organ metadata

--- Virtual Heart (Unity)/Assets/blueprints/organ.cs
+++ Virtual Heart (Unity)/Assets/blueprints/organ.cs
@@ -1,17 +1,12 @@
-﻿using SimpleJSON;
-using System;
-using System.Collections;
-using System.IO;
+﻿using Assets.blueprints;
 using UnityEngine;
 
 
 public class organ : MonoBehaviour {
 
-    public int FMAId = -1;
-
     // Use this for initialization
     void Start () {
-        
+        OrganMetadataManager.OrganMetadata metadata = OrganMetadataManager.Instance.GetOrganMetadata(name);
     }
 	
 	// Update is called once per frame
---
+++ Virtual Heart (Unity)/Assets/blueprints/organMetadataManager.cs
@@ -0,0 +1,64 @@
+﻿using SimpleJSON;
+using System;
+using System.IO;
+using UnityEngine;
+
+namespace Assets.blueprints
+{
+    public class OrganMetadataManager
+    {
+        private const string MetadataFilename = @"D:\Apps\Virtual-Heart\Virtual Heart (Unity)\Assets\blueprints\heart\organ_metadata.json";
+
+        private JSONNode _organMetadata;
+
+        private static OrganMetadataManager _instance;
+        public static OrganMetadataManager Instance
+        {
+            get
+            {
+                return _instance ?? (_instance = new OrganMetadataManager());
+            }
+        }
+
+        /// <summary>
+        /// Reads in the metadata file and saves it
+        /// </summary>
+        private OrganMetadataManager()
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(MetadataFilename))
+                {
+                    var json = reader.ReadToEnd();
+                    _organMetadata = JSON.Parse(json);
+                    Debug.Log("Metadata loaded successfully");
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.Log("Error when reading metadata file: " + e.Message);
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// Gets organ metadata for the specified organ
+        /// </summary>
+        /// <param name="organName">Name of organ to get data for</param>
+        public OrganMetadata GetOrganMetadata(string organName)
+        {
+            Debug.Log("Retrieving metadata for '" + organName + "'");
+            return new OrganMetadata
+            {
+                FMAId = _organMetadata[organName]["FMAId"].Value,
+                Description = _organMetadata[organName]["description"],
+            };
+        }
+
+        public class OrganMetadata
+        {
+            public string FMAId;
+            public string Description;
+        }
+    }
+}
