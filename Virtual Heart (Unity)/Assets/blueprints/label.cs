e058c396a81c4b031c9db011f989d20fb7179865
rcjames1004 <rcjames1004@gmail.com>
2016-01-02 16:29:44 -0800

    Order labels so their lines don't cross
    Crossing lines are ugly. Reposition the labels so their lines don't
    cross.

--- Virtual Heart (Unity)/Assets/blueprints/label.cs
+++ Virtual Heart (Unity)/Assets/blueprints/label.cs
@@ -1,20 +1,112 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
 
 public class label : MonoBehaviour {
 
-    public GameObject Anchor;
+    /// <summary>
+    /// The object this label is anchored to
+    /// </summary>
+    private GameObject _anchor;
+    public GameObject Anchor
+    {
+        get { return _anchor; }
+        set
+        {
+            if (value == null)
+            {
+                Debug.Log("Anchor cannot be null");
+                throw new ArgumentNullException("anchor");
+            }
 
-	// Use this for initialization
-	void Start () {
-        gameObject.GetComponent<TextMesh>().text = Anchor.name;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        // Draw the line between the label and the anchor
-        LineRenderer line = gameObject.GetComponent<LineRenderer>();
-        line.SetPosition(0, transform.position - new Vector3(0, transform.localScale.y, 0)); // Place the line at the bottom of the label
-        line.SetPosition(1, Anchor.transform.position);
+            _anchor = value;
+            gameObject.GetComponent<TextMesh>().text = Anchor.name;
+
+            // Draw the line between the label and the anchor
+            {
+                // Start the line at the label
+                UpdateLinePositionOnLabel();
+
+                // End the line at the closest point on the anchor
+                LinePositionOnAnchor = GetClosestAnchorPoint(referencePoint: LinePositionOnLabel);
+                Line.SetPosition(1, LinePositionOnAnchor);
+            }
+        }
+    }
+
+    /// <summary>
+    /// The line drawn between the label and the line
+    /// </summary>
+    private LineRenderer _line;
+    private LineRenderer Line
+    {
+        get
+        {
+            return _line ?? (_line = gameObject.GetComponent<LineRenderer>());
+        }
+    }
+
+    /// <summary>
+    /// Position of line on label
+    /// </summary>
+    public Vector3 LinePositionOnLabel
+    {
+        get;
+        private set;
+    }
+
+    /// <summary>
+    /// Position of line on anchor
+    /// </summary>
+    public Vector3 LinePositionOnAnchor
+    {
+        get;
+        private set;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.transform.hasChanged)
+            UpdateLinePositionOnLabel();
+    }
+
+    /// <summary>
+    /// Start the line at the bottom of the label
+    /// </summary>
+    private void UpdateLinePositionOnLabel()
+    {
+        // Start the line at the bottom of the label
+        LinePositionOnLabel = transform.position - new Vector3(0, transform.localScale.y, 0);
+        Line.SetPosition(0, LinePositionOnLabel);
+    }
+
+    /// <summary>
+    /// Gets the closes point on the anchor from the given starting point
+    /// </summary>
+    /// <param name="referencePoint">Reference point that shortest distance is calculated with</param>
+    /// <returns>Closest point to the starting point, or the anchor's tranform if the anchor doesn't have child meshes</returns>
+    private Vector3 GetClosestAnchorPoint(Vector3 referencePoint)
+    {
+        // Default with the anchor's transform position
+        Vector3 closestPoint = Anchor.transform.position;
+
+        // Get all the child meshes and add their verties to a single collection
+        List<Vector3> vertices = new List<Vector3>();
+        foreach (MeshFilter meshFilter in Anchor.GetComponentsInChildren<MeshFilter>())
+        {
+            vertices.AddRange(meshFilter.mesh.vertices);
+        }
+
+        // Sort the child vertices and retrieve the closest one
+        if (vertices.Count > 0)
+        {
+            closestPoint = vertices.OrderBy(v => (Anchor.transform.TransformPoint(v) - referencePoint).sqrMagnitude).Take(1).First();
+            closestPoint = Anchor.transform.TransformPoint(closestPoint);
+        }
+
+        return closestPoint;
     }
 }
--- Virtual Heart (Unity)/Assets/blueprints/labelManager.cs
+++ Virtual Heart (Unity)/Assets/blueprints/labelManager.cs
@@ -1,25 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 public class labelManager : MonoBehaviour {
 
     public GameObject label;
 
     private const float DistanceBetweenLabels = 0.06f;
+
     private List<label> _labels;
     private List<label> Labels
     {
         get { return _labels ?? (_labels = new List<label>()); }
+        set { _labels = value; }
     }
 
     public void AddLabelFor(GameObject anchor)
     {
-        // Place the new label above the previous label, or at the bottom if it's the first label
-        Vector3 position = gameObject.transform.position + new Vector3(0, Labels.Count * DistanceBetweenLabels, 0);
-        GameObject lbl = Instantiate(label, position, Quaternion.identity) as GameObject;
+        // Create the label and set it's anchor
+        GameObject lbl = Instantiate(label) as GameObject;
         label labelComponent = lbl.GetComponent<label>();
         labelComponent.Anchor = anchor;
         Labels.Add(labelComponent);
+
+        RepositionLables();
+    }
+
+    /// <summary>
+    /// Reposition labels to decrease the odds their lines cross
+    /// </summary>
+    private void RepositionLables()
+    {
+        // To limit the number of lines that cross
+        // sort the labels based on what they are connecting to,
+        // then set their position based on their order
+        Labels = Labels.OrderBy(l => l.LinePositionOnAnchor.y).ToList();
+        for (int labelIndex = 0; labelIndex < Labels.Count; labelIndex++)
+        {
+            // Place the new label above the previous label, or at the bottom if it's the first label
+            Vector3 position = gameObject.transform.position + new Vector3(0, labelIndex * DistanceBetweenLabels, 0);
+            Labels[labelIndex].transform.position = position;
+        }
     }
 }
