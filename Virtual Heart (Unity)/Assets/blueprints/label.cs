0b0800a3d35cbdc7789cfe8cf994aaad8db83775
rcjames1004 <rcjames1004@gmail.com>
2016-01-02 22:33:41 -0800

    Remove line from label

--- Virtual Heart (Unity)/Assets/blueprints/label.cs
+++ Virtual Heart (Unity)/Assets/blueprints/label.cs
@@ -33,96 +33,7 @@ public class label : MonoBehaviour, ISelectable {
 
             // Add a box collider around the text so we can select it
             gameObject.AddComponent<BoxCollider>();
-
-            // Draw the line between the label and the anchor
-            {
-                // Start the line at the label
-                UpdateLinePositionOnLabel();
-
-                // End the line at the closest point on the anchor
-                LinePositionOnAnchor = GetClosestAnchorPoint(referencePoint: LinePositionOnLabel);
-                Line.SetPosition(1, LinePositionOnAnchor);
-            }
         }
-    }
-
-    /// <summary>
-    /// The line drawn between the label and the line
-    /// </summary>
-    private LineRenderer _line;
-    private LineRenderer Line
-    {
-        get
-        {
-            return _line ?? (_line = gameObject.GetComponent<LineRenderer>());
-        }
-    }
-
-    /// <summary>
-    /// Position of line on label
-    /// </summary>
-    public Vector3 LinePositionOnLabel
-    {
-        get;
-        private set;
-    }
-
-    /// <summary>
-    /// Position of line on anchor
-    /// </summary>
-    public Vector3 LinePositionOnAnchor
-    {
-        get;
-        private set;
-    }
-
-    void Start()
-    {
-        Line.enabled = false;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (gameObject.transform.hasChanged)
-            UpdateLinePositionOnLabel();
-    }
-
-    /// <summary>
-    /// Start the line at the bottom of the label
-    /// </summary>
-    private void UpdateLinePositionOnLabel()
-    {
-        // Start the line at the bottom of the label
-        LinePositionOnLabel = transform.position - new Vector3(0, transform.localScale.y, 0);
-        Line.SetPosition(0, LinePositionOnLabel);
-    }
-
-    /// <summary>
-    /// Gets the closes point on the anchor from the given starting point
-    /// </summary>
-    /// <param name="referencePoint">Reference point that shortest distance is calculated with</param>
-    /// <returns>Closest point to the starting point, or the anchor's tranform if the anchor doesn't have child meshes</returns>
-    private Vector3 GetClosestAnchorPoint(Vector3 referencePoint)
-    {
-        // Default with the anchor's transform position
-        Vector3 closestPoint = Anchor.transform.position;
-
-        // Get all the child meshes and add their verties to a single collection
-        List<Vector3> vertices = new List<Vector3>();
-        foreach (MeshFilter meshFilter in Anchor.GetComponentsInChildren<MeshFilter>())
-        {
-            vertices.AddRange(meshFilter.mesh.vertices);
-        }
-
-        // Sort the child vertices and retrieve the closest one
-        if (vertices.Count > 0)
-        {
-            closestPoint = vertices.OrderBy(v => (Anchor.transform.TransformPoint(v) - referencePoint).sqrMagnitude).Take(1).First();
-            closestPoint = Anchor.transform.TransformPoint(closestPoint);
-        }
-
-        return closestPoint;
     }
 
     #region ISelectable
@@ -130,7 +41,6 @@ public class label : MonoBehaviour, ISelectable {
     public void Select()
     {
         SetTextColor(SelectedColor);
-        Line.enabled = true;
         ISelectable selectable = Anchor.GetComponent<ISelectable>();
         if (selectable != null)
             selectable.Select();
@@ -139,7 +49,6 @@ public class label : MonoBehaviour, ISelectable {
     public void Deselect()
     {
         SetTextColor(Color.black);
-        Line.enabled = false;
         ISelectable selectable = Anchor.GetComponent<ISelectable>();
         if (selectable != null)
             selectable.Deselect();
--- Virtual Heart (Unity)/Assets/blueprints/labelManager.cs
+++ Virtual Heart (Unity)/Assets/blueprints/labelManager.cs
@@ -20,27 +20,11 @@ public class labelManager : MonoBehaviour {
     {
         // Create the label and set it's anchor
         GameObject lbl = Instantiate(label) as GameObject;
+        Vector3 position = gameObject.transform.position + new Vector3(0, Labels.Count * DistanceBetweenLabels, 0);
+        lbl.transform.position = position;
+
         label labelComponent = lbl.GetComponent<label>();
         labelComponent.Anchor = anchor;
         Labels.Add(labelComponent);
-
-        RepositionLables();
-    }
-
-    /// <summary>
-    /// Reposition labels to decrease the odds their lines cross
-    /// </summary>
-    private void RepositionLables()
-    {
-        // To limit the number of lines that cross
-        // sort the labels based on what they are connecting to,
-        // then set their position based on their order
-        Labels = Labels.OrderBy(l => l.LinePositionOnAnchor.y).ToList();
-        for (int labelIndex = 0; labelIndex < Labels.Count; labelIndex++)
-        {
-            // Place the new label above the previous label, or at the bottom if it's the first label
-            Vector3 position = gameObject.transform.position + new Vector3(0, labelIndex * DistanceBetweenLabels, 0);
-            Labels[labelIndex].transform.position = position;
-        }
     }
 }
