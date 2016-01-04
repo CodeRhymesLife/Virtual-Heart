370bc32d0b922b72ec6c916ef4e27588acc85cd9
rcjames1004 <rcjames1004@gmail.com>
2016-01-04 02:38:05 -0800

    Create object and attach organ script based on metadata file

---
+++ Virtual Heart (Unity)/Assets/Scripts/organ.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using Assets.blueprints;
+
+namespace Assets.Scripts
+{
+    public class organ : MonoBehaviour
+    {
+
+        public OrganMetadataManager.OrganMetadata Metadata
+        {
+            get;
+            set;
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
--- Virtual Heart (Unity)/Assets/Scripts/organLoader.cs
+++ Virtual Heart (Unity)/Assets/Scripts/organLoader.cs
@@ -3,14 +3,31 @@ using System.Collections;
 using System.Collections.Generic;
 using Assets.blueprints;
 
-public class organLoader : MonoBehaviour {
+namespace Assets.Scripts
+{
+    public class organLoader : MonoBehaviour {
 
-    // Use this for initialization
-    void Start() {
-        OrganMetadataManager.OrganMetadata[] organPartContainerMetadata = OrganMetadataManager.LoadMetadata();
-        foreach(OrganMetadataManager.OrganMetadata metadata in organPartContainerMetadata)
+        // Use this for initialization
+        void Start() {
+            OrganMetadataManager.OrganMetadata[] organPartContainerMetadata = OrganMetadataManager.LoadMetadata();
+            foreach (OrganMetadataManager.OrganMetadata metadata in organPartContainerMetadata)
+            {
+                // Give the user the option to load an organ
+
+                // Temporarily load each one
+                LoadOrgan(metadata);
+            }
+        }
+
+        private void LoadOrgan(OrganMetadataManager.OrganMetadata organMetadata)
         {
-            // Give the user the option to load an organ
+            Debug.Log("Creating organ for: " + organMetadata.Name);
+            GameObject organObj = new GameObject(organMetadata.Name);
+            organ script = organObj.AddComponent<organ>();
+            script.Metadata = organMetadata;
+            Instantiate(organObj);
+
+            // Size properly
         }
     }
 }
